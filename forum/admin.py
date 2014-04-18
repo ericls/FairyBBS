df3f905b8eff098260ff510e491baa3571a8d690
eric lee <eric@v7.(none)>
2014-04-18 18:52:36 +0800

    fix admin

--- forum/admin.py
+++ forum/admin.py
@@ -3,12 +3,17 @@ from forum.models import topic, mention, notification, post, node
 # Register your models here.
 
 
+class post_inline(admin.StackedInline):
+    model = post
+    exclude = ['user', 'content_rendered']
+    extra = 0
+
+
 class topic_admin(admin.ModelAdmin):
     list_display = ('title', 'time_created', 'user', 'click', 'last_replied')
     list_filter = ('time_created',)
-    #readonly_fields = ('user',)
-    search_fields = ['title', 'user']
+    search_fields = ['title', 'user__username']
+    inlines = [post_inline]
 
 admin.site.register(topic, topic_admin)
-admin.site.register(post)
 admin.site.register(node)
