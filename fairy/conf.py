774915116d22617f0e8b19c6a2ab63349f533a44
ericls <etkids@163.com>
2014-08-14 11:37:06 +0800

    fix stat error

--- fairy/conf.py
+++ fairy/conf.py
@@ -13,7 +13,7 @@ nodes = node.objects.all()
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 UPLOAD_PATH = os.path.join(BASE_DIR, 'static/upload')
 user_count = profile.objects.count()
-topic_count = topic.objects.count()
-post_count = post.objects.count()
+topic_count = topic.objects.filter(deleted=False).count()
+post_count = post.objects.filter(deleted=False).count()
 
 site_off = False
